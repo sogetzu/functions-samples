1b320fac7760d4680e2751e264c4c5ebbdf12cf8
sogetzu <francisp.0925@gmail.com>
2018-08-18 21:05:35 +0800

    Update index.js
    Firebase has changed the URLs where they host Cloud functions.

--- google-sheet-sync/functions/index.js
+++ google-sheet-sync/functions/index.js
@@ -36,7 +36,7 @@ const CONFIG_SHEET_ID = functions.config().googleapi.sheet_id;
 const CONFIG_DATA_PATH = functions.config().watchedpaths.data_path;
 
 // The OAuth Callback Redirect.
-const FUNCTIONS_REDIRECT = `https://${process.env.GCLOUD_PROJECT}.firebaseapp.com/oauthcallback`;
+const FUNCTIONS_REDIRECT = `https://us-central1-${process.env.GCLOUD_PROJECT}.cloudfunctions.net/oauthcallback`;
 
 // setup for authGoogleAPI
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
